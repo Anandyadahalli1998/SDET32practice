6fdfee874e123799001666d49dfdc1022b07cce1
DELL <DELL@DESKTOP-BN6R26C>
2022-04-12 11:45:49 +0530

    hello

---
+++ com.crm.vtiger.SDET32/src/test/java/com/crm/vtiger/SDET32_GitPractice/ClassA.java
@@ -0,0 +1,11 @@
+package com.crm.vtiger.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+
+	@Test
+	public void abc() {
+		System.out.println("push code");
+	}
+}
