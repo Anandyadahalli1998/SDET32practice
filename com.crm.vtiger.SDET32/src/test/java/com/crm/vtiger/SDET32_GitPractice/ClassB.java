2b14a7d21e8dbc56c9187fc07925a2dd27ef5d4c
DELL <DELL@DESKTOP-BN6R26C>
2022-04-12 12:17:51 +0530

    commit 2

---
+++ com.crm.vtiger.SDET32/src/test/java/com/crm/vtiger/SDET32_GitPractice/ClassB.java
@@ -0,0 +1,11 @@
+package com.crm.vtiger.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+	@Test
+	public void demo2() {
+		System.out.println("change and push code");
+	}
+
+}
